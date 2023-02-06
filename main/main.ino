5226568cf1efa817671a925fe6387c51644a4f88
Payton Gilbertson <45208074+paytonGilbertson@users.noreply.github.com>
2023-02-06 15:52:43 -0600

    Finished up basic pseudocode in main.ino

--- main/main.ino
+++ main/main.ino
@@ -1,9 +1,32 @@
-void setup() {
-  // put your setup code here, to run once:
+// DEFINE CONST VARIABLES
 
+//PhotoResistor Pin
+const int photoresistorPin = 0; // A0 reads photoresistor value
+
+//LED Pin
+const int ledPin = 9;   //  pin 9 outputs led brightness 
+
+
+void setup()
+{
+  // set up OUTPUT pins for LED, SERVOS, SPEAKER, etc.
+  pinMode(ledPin, OUTPUT); 
+  Serial.begin(9600);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
 
+// All functions in LOOP regarding sensors will call their own "reaction" functions 
+// so they do not need to be referenced here
+void loop()
+{
+ // function call for PHOTORESISTOR and LED action/reaction
+ checkLightLevels();
+
+ // function call for ULTRASONIC sensor
+ checkForMotion();
+
+// function calls for ULTRASONIC reactions (different for different distances)
+  checkDistance();
 }
+
+ 
