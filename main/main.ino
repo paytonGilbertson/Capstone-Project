a3bff65b0af24c58e21685da0d00da76f3f9fdc9
Payton Gilbertson <45208074+paytonGilbertson@users.noreply.github.com>
2023-02-07 11:41:07 -0600

    libraries, pins, and code for photoresistor added

--- main/main.ino
+++ main/main.ino
@@ -1,32 +1,55 @@
+// Information needed for the "Servo" library to control motors
+#include <Servo.h>
+// Information needed for the "Talkie" library which will control the voice
+#include <Talkie.h>
+#include <TalkieUtils.h>
+#include <Vocab_US_Large.h>
+#include <Vocab_Special.h>
+
+//--------------------------------------------------
+
 // DEFINE CONST VARIABLES
+// PhotoResistor Pin
+const int photoresistorPin = 0;  // A0 reads photoresistor value
 
-//PhotoResistor Pin
-const int photoresistorPin = 0; // A0 reads photoresistor value
+// LED Pin
+const int ledPin = 3;  //  pin 3 outputs led brightness
 
-//LED Pin
-const int ledPin = 9;   //  pin 9 outputs led brightness 
+// SERVOs 
+// Servo objects are defined here, attached to pins in setup() function
+Servo rShoulder;
+Servo lShoulder;
+Servo rElbow;
+Servo lElbow;
+Servo mouth;
 
+//--------------------------------------------------
 
-void setup()
-{
+// Run only once at the beginning of the program (on plugin of the animatronic)
+void setup() {
   // set up OUTPUT pins for LED, SERVOS, SPEAKER, etc.
-  pinMode(ledPin, OUTPUT); 
+  pinMode(ledPin, OUTPUT);
+
+  rShoulder.attach(10);
+  lShoulder.attach(6);
+  rElbow.attach(9);
+  lElbow.attach(5);
+  mouth.attach(4);
+
   Serial.begin(9600);
 }
 
+//--------------------------------------------------
 
-// All functions in LOOP regarding sensors will call their own "reaction" functions 
+// All functions in LOOP regarding sensors will call their own "reaction" functions
 // so they do not need to be referenced here
-void loop()
-{
- // function call for PHOTORESISTOR and LED action/reaction
- checkLightLevels();
+void loop() {
+  // function call for PHOTORESISTOR and LED action/reaction
+  checkLightLevels();
 
- // function call for ULTRASONIC sensor
- checkForMotion();
+  // function call for ULTRASONIC sensor
+  checkForMotion();
 
-// function calls for ULTRASONIC reactions (different for different distances)
+  // function calls for ULTRASONIC reactions (different for different distances)
   checkDistance();
 }
-
- 
---
+++ main/photoresistor.ino
@@ -0,0 +1,16 @@
+void checkLightLevels() {
+  int lightLevel = analogRead(photoresistorPin);  // Read the lightlevel
+
+  lightLevel = map(lightLevel, 400, 900, 0, 255);
+  // Adjust the value 0 to 900 to span 0 to 255
+
+  // Print out the lightLevel value to check threshold is correct
+  Serial.println(lightLevel);
+
+  // Turn light off if there is light, turn it on based on amount of light
+  if (lightLevel < 100) {
+    analogWrite(ledPin, LOW);
+  } else {
+    analogWrite(ledPin, HIGH);  // Write the value
+  }
+}
